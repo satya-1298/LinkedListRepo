ef996bd1ff10695d4a27f35d72bdd7e1d90a6482
unknown <satya-1298>
2023-03-27 18:53:47 +0530

    Create LinkedList

---
+++ LinkedList/List.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListdemo
+{
+    public class List
+    {
+        public Node head;
+        public void add (int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else 
+            {
+                Node temp = head;
+                while(temp.next != null) 
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+
+            }
+            Console.WriteLine("{0} inserted into Linked List",data);
+            
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if(temp==null)
+            {
+                Console.WriteLine("LinkedList is Empty");
+            }
+            while(temp!=null) 
+            {
+                Console.Write(temp.data+" ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
