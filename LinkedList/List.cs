f1335767dc095bbf87c64a96871152c8d4c1db73
unknown <satya-1298>
2023-03-27 19:38:20 +0530

    Create Linkedlist

---
+++ LinkedList/List.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListdemo
+{
+    public class List
+    {
+        public Node head;
+        public void add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+
+            }
+            Console.WriteLine("{0} inserted into Linked List", data);
+
+        }
+        public void AddReverseOrder(int data)
+        {
+            Node head;
+            Node newNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                this.head = newNode;
+                this.head.next = temp;
+            }
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is Empty");
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListdemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List linkedlist = new List();
+            //linkedlist.add(30);
+            //linkedlist.add(70);
+            //linkedlist.add(56);
+           
+            linkedlist.AddReverseOrder(70);
+            linkedlist.AddReverseOrder(30);
+            linkedlist.AddReverseOrder(56);
+            linkedlist.Display();
+
+        }
+    }
+}
