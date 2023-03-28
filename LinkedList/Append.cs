49ba7b89d464ac87be2ceacf5924bfb03bfc4805
unknown <satya-1298>
2023-03-28 16:35:53 +0530

    Appending elements

---
+++ LinkedList/Append.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListdemo
+{
+    public class Append
+    {
+        
+         public Node head;
+       public   void AddNodetoFront(int data)
+        {
+            
+            Node n = new Node(data);
+            if(this.head == null) 
+            {
+               
+                this.head = n;
+            }
+            else 
+            {
+                Node temp = head;
+                head = n;
+                head.next = temp;
+            }
+            Console.WriteLine(data);
+            
+
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is Empty");
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListdemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List l = new List();
+            //l.add(70);
+            //l.add(30);
+            //l.add(56);
+            
+            //l.AddReverseOrder(70);
+            //l.AddReverseOrder(30);
+            //l.AddReverseOrder(56);
+            //l.Display();
+            Append a = new Append();
+            a.AddNodetoFront(30);
+            a.AddNodetoFront(40);
+            a.AddNodetoFront(34);
+            a.Display();
+
+        }
+    }
+}
