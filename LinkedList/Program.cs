eec818feee7632210291bc7394d2edc3367d6d7e
unknown <satya-1298>
2023-03-28 22:35:08 +0530

    Insert a Element

---
+++ LinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListdemo1
+{
+    public  class Program
+    {
+        public static void Main(string[] args)
+        {
+            List l= new List();
+            l.add(56);
+            l.add(30);
+            l.add(70);
+            l.Display();
+            l.InsertNode(2, 40);
+            l.Display();
+        }
+    }
+}
