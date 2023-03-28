9bbcb60e85ae146d50ba1170d7fb38e719d6f46b
unknown <satya-1298>
2023-03-28 20:14:49 +0530

    Insert Between Elements

---
+++ LinkedList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListdemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List l = new List();
+            l.add(70);
+            l.add(56);
+            l.Display();
+            l.InsertAtPosition(1, 30);
+            l.Display();
+
+
+        }
+    }
+}
