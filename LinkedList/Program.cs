a22f897c7e29237aa68d718b0f7e604b52785ad8
unknown <satya-1298>
2023-03-28 22:27:39 +0530

    Searching a Element

---
+++ LinkedList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListdemo1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List l = new List();
+            l.add(70);
+            l.add(56);
+            l.Display();
+            Console.WriteLine("\n");
+            l.InsertAtPosition(1, 30);
+            l.Display();
+            //Console.WriteLine("\n");
+            //l.DeleteFirstNode();
+            //l.Display();
+            //Console.WriteLine("\n");
+            //l.DeleteLastNode();
+            //l.Display();
+            Console.WriteLine("\n");
+            l.SearchNode(70);
+            l.Display();
+        }
+
+    }
+}
