13acc6f6b5ce157bc3441d7c783b06ce0bdac93e
unknown <satya-1298>
2023-03-29 15:17:12 +0530

    List in Ascending order

---
+++ LinkedList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListdemo1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List l = new List();
+            l.add(56);
+            l.add(30);
+            l.add(70);
+            l.Display();
+            Console.WriteLine("\n");
+            l.InsertNode(2, 40);
+            l.Display();
+            Console.WriteLine("\n");
+            l.RemoveNode(40);
+            l.Display();
+            Console.WriteLine("\n");
+            l.size();
+            Console.WriteLine("\n");
+            l.sort();
+            l.Display();
+        }
+    }
+}
